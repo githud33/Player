1a680a93230374ab5581c8d2c5a3cd77b112a0ee
githud33 <githud33@gmail.com>
2026-06-06 18:08:47 +0700

    Delete Player directory

--- Player/ล้างแคช/เครื่องเล่น-js-m3u8.js
+++
@@ -1,4 +0,0 @@
-var script = document.createElement('script');
-script.src = 'https://githud33.github.io/Player/ไฟล์หลัก/เครื่องเล่น-js-m3u8.js?t=' + new Date().getTime();
-script.defer = true;
-document.head.appendChild(script);
