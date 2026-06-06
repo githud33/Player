8627c400c17eae1597905956d1bf8074f47849dd
githud33 <githud33@gmail.com>
2026-06-06 16:41:55 +0700

    Delete

--- ล้างแคช/เครื่องเล่น-js-m3u8.js
+++
@@ -1,5 +0,0 @@
-var script = document.createElement('script');
-// เธเธตเนเธ—เธฒเธเนเธเธซเธฒเนเธเธฅเนเธ•เธฑเธงเธเธฃเธดเธเนเธเนเธเธฅเน€เธ”เธญเธฃเน core
-script.src = 'https://githud33.github.io/Player/เนเธเธฅเนเธซเธฅเธฑเธ/เน€เธเธฃเธทเนเธญเธเน€เธฅเนเธ-js-m3u8.js?t=' + new Date().getTime();
-script.defer = true;
-document.head.appendChild(script);
