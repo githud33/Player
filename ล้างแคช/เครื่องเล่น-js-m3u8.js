8d8aa19669760bdeb1b2211759275aeb633dc7ce
githud33 <githud33@gmail.com>
2026-06-06 18:09:39 +0700

    Add files via upload

---
+++ ล้างแคช/เครื่องเล่น-js-m3u8.js
@@ -0,0 +1,4 @@
+var script = document.createElement('script');
+script.src = 'https://githud33.github.io/Player/ไฟล์หลัก/เครื่องเล่น-js-m3u8.js?t=' + new Date().getTime();
+script.defer = true;
+document.head.appendChild(script);
