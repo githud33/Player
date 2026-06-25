db5bcac9c5f00f3595d14927db103e0f84a396f1
githud33 <githud33@gmail.com>
2026-06-25 15:33:35 +0700

    Create เปิดตัวเล่น=ru.js

---
+++ เปิดตัวเล่น=ru.js
@@ -0,0 +1,12 @@
+var video = document.getElementById("main-video");
+
+// วิ่งไปหาแท็กที่มี id="m3" หรือ id="mk" หรือ id="tx" ทั้งหมดมาลูปเพื่อลบออก
+document.querySelectorAll("#mp, #m3, #mk, #tx").forEach(function(mediaSource) {
+    mediaSource.remove();
+});
+
+// สั่งโหลดวิดีโอใหม่และเล่นทันที (ยุบรวมเหลือชุดเดียวพอครับ)
+if (video) {
+    video.load();
+   // video.play(); // สั่งให้เล่นวิดีโอตัวที่เหลืออยู่ (id="mp") ทันที ***ถ้าเปิดแท็กตัวนี้ เริ่มเล่นทันทีโดยที่คนดูไม่ต้องกดปุ่ม Play สามารถเพิ่มคำสั่ง .play()
+}
